d62689bf41e3b50f39eb714290a74df1a32dd053
RuinsGit <museyibli.ruhin@gmail.com>
2025-02-06 05:05:34 -0800

    f

--- resources/views/back/includes/footer.blade.php
+++ resources/views/back/includes/footer.blade.php
@@ -1,13 +1,11 @@
 <footer class="footer" style="background-color: {{ $themeSettings->footer_bg_color }}; color: {{ $themeSettings->footer_text_color }};">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6">
-                {!! $themeSettings->footer_text !!}
-            </div>
-            <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Designed <i class="mdi mdi-heart text-danger"></i> by 
-                    <a href="https://166tech.az" target="_blank" style="color: {{ $themeSettings->footer_text_color }};">166 Tech</a>
+    <div class="container-fluid" style="height: 100%;">
+        <div class="row" style="height: 100%;">
+        
+            <div class="" style="display: flex; justify-content: flex-end; align-items: center; height:100%;"> 
+                <div class="" style="height: 40px; width: 400px; margin-right: 0;">
+                    {!! $themeSettings->footer_text !!}
+                </div>
                 </div>
             </div>
         </div>
